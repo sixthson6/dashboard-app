c6bf5582fedc22c7a16bd27b2445c497195d45ae
sixthson6 <sudais2408@gmail.com>
2025-09-26 10:51:13 +0000

    feat: Add Hompage and Dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import { Inter } from 'next/font/google';
+
+export default function RootLayout(
+  {children,}: { children: React.ReactNode;}
+) 
+{
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={'${inter.className} antialiased'}>{children}</body>
     </html>
   );
 }
